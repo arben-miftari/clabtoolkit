1213d022714af2ca9c922f0da902f27f93b28e95
Miftari Arben <arbenmiftari924@gmail.com>
2025-07-03 09:07:07 +0200

    added some print for log output

--- shell_script/organize_bids_folder.py
+++ shell_script/organize_bids_folder.py
@@ -8,12 +8,19 @@ def main():
     parser.add_argument("id_list", help="Path to list of IDs to process")
     args = parser.parse_args()
 
+    print("STARTED")
+    
+    print(f"INPUT_FOLDER={args.input_dir}")
+    print(f"OUTPUT_FOLDER={args.output_dir}")
+    print(f"ID_LIST={args.id_list}")
 
     cltdicomtools.org_conv_dicoms(
         in_dic_dir=args.input_dir,
         out_dic_dir=args.output_dir,
         ids_file=args.id_list
     )
+
+    print("FINISHED")
 
 if __name__ == "__main__":
     main()
