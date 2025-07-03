4abc210ce120108e784742f988163ff9caec46ed
Miftari Arben <arbenmiftari924@gmail.com>
2025-07-03 09:01:55 +0200

    update mistake

--- shell_script/organize_bids_folder.py
+++ shell_script/organize_bids_folder.py
@@ -1,5 +1,5 @@
 import argparse
-import clabtoolkit.dictomtools as cltdicomtools
+import clabtoolkit.dicomtools as cltdicomtools
 
 def main():
     parser = argparse.ArgumentParser(description="Organize DICOM folder.")
@@ -7,6 +7,7 @@ def main():
     parser.add_argument("output_dir", help="Path to output directory")
     parser.add_argument("id_list", help="Path to list of IDs to process")
     args = parser.parse_args()
+
 
     cltdicomtools.org_conv_dicoms(
         in_dic_dir=args.input_dir,
